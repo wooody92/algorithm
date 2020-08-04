6c6af2826e94fb19325b4279ae1b126ad252bdc3
wooody92 <pkower4@naver.com>
2020-08-05 00:23:34 +0900

    [programmers - 42746] 가장 큰 수
    - 좋은 생각이 나지 않아 구글링하였다. `compareTo` 메서드를 적절히 활용한 좋은 문제같다.
    - `compareTo` 메서드를 이용하여 두 String 객체를 더해서 큰 수에 우선순위가 있도록 정렬한다.

---
+++ src/main/java/programmers/level_2/biggest_number_42746/Solution.java
@@ -0,0 +1,31 @@
+package programmers.level_2.biggest_number_42746;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public String solution(int[] numbers) {
+        String[] num = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            num[i] = String.valueOf(numbers[i]);
+        }
+
+        // 두 String 객체를 더해서 큰 수에 우선순위가 있도록 정렬한다.
+        Arrays.sort(num, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if (num[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < num.length; i++) {
+            sb.append(num[i]);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] numbers = {3, 39, 34, 5111, 51, 529, 9, 990, 99, 90, 908, 1, 1000};
+        System.out.println(solution.solution(numbers));
+    }
+}
