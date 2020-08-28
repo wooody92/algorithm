8ba739f131494791c056ed8925d167bc5ea10239
wooody92 <pkower4@naver.com>
2020-08-28 21:34:53 +0900

    [programmers - 17683] 방금그곡 - kakao 2018 - 2
    - 처음 풀이에서 몇가지 테스트케이스에 통과하지 못했다. 다른 사람의 풀이를 보니 `C#`의 경우 `c`로 치환하여 풀이하였다.
    - 데이터를 저장하는 `Music` 클래스와 `C# -> c` 치환을 이용하여 다시 풀었다.

--- src/main/java/programmers/level_2/shazam_17683/Solution.java
+++ src/main/java/programmers/level_2/shazam_17683/Solution.java
@@ -1,132 +1,90 @@
 package programmers.level_2.shazam_17683;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
 
-    class Music {
-        private int minutes;
-        private String code;
+    /**
+     * music data Class
+     */
+    public class Music {
         private String title;
+        private String code;
+        private int minutes;
 
-        public Music(int minutes, String code, String title) {
-            this.minutes = minutes;
-            this.code = code;
+        public Music(String title, String code, int minutes) {
             this.title = title;
-        }
-
-        public int getMinutes() {
-            return minutes;
+            this.code = code;
+            this.minutes = minutes;
         }
 
         public String getTitle() {
             return title;
         }
 
-        public String getCode(int count, String code) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < count; i++) {
-                if (code.charAt(i) == '#') {
-                    continue;
-                }
-                if (code.charAt(i + 1) == '#') {
-                    sb.append(code.charAt(i) + "#");
-                    count++;
-                    continue;
-                }
-                sb.append(code.charAt(i));
-            }
-            return sb.toString();
+        public int getMinutes() {
+            return minutes;
         }
 
         public String getTotalCode() {
             StringBuilder sb = new StringBuilder();
-            int codeLength = getCodeLength(code);
-
-            if (minutes < codeLength) {
-                return getCode(minutes, code);
-            }
-
-            int times = minutes / codeLength;
-            int rest = minutes % codeLength;
+            int times = minutes / code.length();
+            int rest = minutes % code.length();
             for (int i = 0; i < times; i++) {
                 sb.append(code);
             }
-            sb.append(getCode(rest, code));
+            for (int i = 0; i < rest; i++) {
+                sb.append(code.charAt(i));
+            }
             return sb.toString();
-        }
-
-        public int getCodeLength(String code) {
-            return code.length() - getHashCount(code);
-        }
-
-        public int getHashCount(String code) {
-            int count = 0;
-            for (int i = 0; i < code.length(); i++) {
-                if (code.charAt(i) == '#') {
-                    count++;
-                }
-            }
-            return count;
-        }
-
-        // m : ABC, condition : ABC#의 경우는 false
-        public boolean isContain(String m) {
-            String totalCode = getTotalCode();
-            if (totalCode.contains(m)) {
-                int nextIndex = totalCode.indexOf(m) + m.length();
-                if (totalCode.charAt(nextIndex) != '#') {
-                    return true;
-                }
-            }
-            return false;
         }
     }
 
-    public String solution(String m, String[] musicInformation) {
-        Music[] music = new Music[musicInformation.length];
-        int hour = 0;
-        int minute = 0;
-        for (int i = 0; i < musicInformation.length; i++) {
-            String[] data = musicInformation[i].split(",");
+    public String solution(String melody, String[] musicInfo) {
+
+        // init
+        melody = replaceCharacter(melody);
+        Music[] musicList = new Music[musicInfo.length];
+        for (int i = 0; i < musicInfo.length; i++) {
+            String[] data = musicInfo[i].split(",");
             String[] startTime = data[0].split(":");
-            String[] lastTime = data[1].split(":");
-            hour = Integer.parseInt(lastTime[0]) - Integer.parseInt(startTime[0]);
-            minute = Integer.parseInt(lastTime[1]) - Integer.parseInt(startTime[1]);
-            music[i] = new Music((hour * 60) + minute, data[3], data[2]);
+            String[] endTime = data[1].split(":");
+            int hour = Integer.parseInt(endTime[0]) - Integer.parseInt(startTime[0]);
+            int minute = Integer.parseInt(endTime[1]) - Integer.parseInt(startTime[1]);
+            int minutes = (hour * 60) + minute;
+            musicList[i] = new Music(data[2], replaceCharacter(data[3]), minutes);
         }
 
-        // 주어진 코드 m과 일치하는 Music을 모두 찾는다.
-        List<Music> myMusicList = new ArrayList<>();
-        for (Music musicInfo : music) {
-            if (musicInfo.isContain(m)) {
-                myMusicList.add(musicInfo);
+        // logic
+        // playingTime이 큰 경우에만 고려하여, 같은 재생 시간에서는 먼저 입력된 값을 저장한다.
+        int comparator = 0;
+        String result = "(None)";
+        for (Music music : musicList) {
+            String totalCode = music.getTotalCode();
+            int playingTime = music.getMinutes();
+            if (totalCode.contains(melody) && comparator < playingTime) {
+                result = music.getTitle();
+                comparator = playingTime;
             }
         }
+        return result;
+    }
 
-        // 조건에 일치하는 Music의 갯수에 따라 제목을 반환한다.
-        if (myMusicList.size() == 1) {
-            return myMusicList.get(0).getTitle();
-        }
-        // 조건에 일치하는 Music이 다수일경우 시간을 비교하고, 시간도 일치할 경우 먼저 입력된 Music을 반환한다.
-        if (myMusicList.size() > 1) {
-            int lastIndex = myMusicList.size() - 1;
-            Music myMusic = myMusicList.get(lastIndex);
-            for (int i = lastIndex; i > 0 ; i--) {
-                if (myMusicList.get(i).getMinutes() < myMusicList.get(i - 1).getMinutes()) {
-                    myMusic = myMusicList.get(i - 1);
-                }
-            }
-            return myMusic.getTitle();
-        }
-        return "`(None)`";
+    /**
+     * '#'때문에 character length의 계산이 복잡해져서 치환한다.
+     */
+    private String replaceCharacter(String melody) {
+        return melody.replaceAll("C#", "c")
+            .replaceAll("D#", "d")
+            .replaceAll("E#", "e")
+            .replaceAll("F#", "f")
+            .replaceAll("G#", "g")
+            .replaceAll("A#", "a")
+            .replaceAll("B#", "b");
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         String m = "ABC";
-        String[] musicInfo = {"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"};
+        String[] musicInfo = {"10:30,10:50,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"};
         System.out.println(solution.solution(m, musicInfo));
     }
 }
